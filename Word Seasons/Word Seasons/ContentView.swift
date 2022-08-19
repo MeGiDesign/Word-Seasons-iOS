633dc8e5e6443e16ec0cd8e59cb20979c94cbb7a
Okan Aslan <okanaslan.08@gmail.com>
2022-08-19 17:31:45 +0300

    init

---
+++ Word Seasons/Word Seasons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Word Seasons
+//
+//  Created by Okan Aslan on 19.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Word Seasons/Word Seasons/Word_SeasonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_SeasonsApp.swift
+//  Word Seasons
+//
+//  Created by Okan Aslan on 19.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Word_SeasonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
